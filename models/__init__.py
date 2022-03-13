705e464c44eeff654dd9a500bc1deb8c0338f268
Miguel <miguelhatrick@gmail.com>
2022-03-13 04:01:16 -0300

    mods

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from . import career, course, course_student, course_type, exam, exam_student
+from . import course, career, course_student, course_type, exam, exam_student
 from . import pos_order, sale_order, res_partner, res_stock_change_product_qty
